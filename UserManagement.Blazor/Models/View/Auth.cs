5e2aaecd3d19bada68f64b4711da1a94a0172adf
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2025-08-23 17:59:43 +0100

    Bundling of static assets + login form errors

--- UserManagement.Blazor/Models/View/Auth.cs
+++ UserManagement.Blazor/Models/View/Auth.cs
@@ -1,7 +1,13 @@
-﻿namespace UserManagement.Blazor.Models.View;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Blazor.Models.View;
 
 public class Auth
 {
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
 }
